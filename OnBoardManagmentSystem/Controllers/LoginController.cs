be442aa4c65ebce0eced40123dfb65ed20b30bb0
Darshan.Jain <Darshan.Jain@ASIA18861>
2018-04-23 17:57:42 +0530

    Added LoginController, Implemented RoleBasedAuthentication, Created Mentor Home Page

---
+++ OnBoardManagmentSystem/Controllers/LoginController.cs
@@ -0,0 +1,41 @@
+﻿using OnBoardManagement.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace OnBoardManagmentSystem.Controllers
+{
+    public class LoginController : Controller
+    {
+        OnBoardDb ctx = new OnBoardDb();
+        // GET: Login
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(User login)
+        {
+            User result = ctx.Users.ToList().FirstOrDefault(m => m.U_Username == login.U_Username && m.U_Password == login.U_Password);
+            if (result == null)
+            {
+                ViewBag.msg = "Invalid Username or password";
+                return View();
+            }
+            else if (result.U_Role == "admin")
+            {
+                //admin
+                return RedirectToAction("Index", "Home");
+            }
+            else
+            {
+                Mentor mentor = ctx.Mentors.FirstOrDefault(m => result.U_Username.Equals(m.M_Name));
+                return RedirectToAction("View", "Mentor", new { id = mentor.M_Id });
+
+            }
+        }
+    }
+}
--- OnBoardManagmentSystem/Controllers/MentorController.cs
+++ OnBoardManagmentSystem/Controllers/MentorController.cs
@@ -1,4 +1,4 @@
-﻿﻿using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Data;
 using System.Data.Entity;
@@ -9,19 +9,69 @@ using System.Web.Mvc;
 using Kendo.Mvc.Extensions;
 using Kendo.Mvc.UI;
 using OnBoardManagement.Models;
+using OnBoardManagmentSystem.Models;
 
 namespace OnBoardManagmentSystem.Controllers
 {
+
     public class MentorController : Controller
     {
         private OnBoardDb db = new OnBoardDb();
-
-        public ActionResult Index()
+        OnBoardDb ctx = new OnBoardDb();
+        public ActionResult Index(int id)
         {
             return View();
         }
 
+        public ActionResult View(int id)
+        {
+            Mentor mentor = ctx.Mentors.FirstOrDefault(m => m.M_Id == id);
+           Project project= ctx.Projects.FirstOrDefault(m => m.M_Id == mentor.M_Id);
+            
+            ViewBag.mentor = mentor;
+            ViewBag.project = project;
+            List<OnBoarder> OnBoarders = ctx.OnBoarders.Where(m => m.M_Id == id).ToList();
+            List<Models.OnBoarderDetailModel> details = new List<OnBoarderDetailModel>();
+            foreach (OnBoarder ob in OnBoarders)
+            {
+                OnBoarderDetailModel obm = new OnBoarderDetailModel();
+                obm.Name = ob.O_Name;
+                obm.OnboarderId = ob.O_Id;
+                List<ProjectAssignment> rotations = ctx.ProjectAssignments.Where(m => m.O_Id == ob.O_Id).ToList();
 
+                foreach (ProjectAssignment r in rotations)
+                {
+                    if (obm.Rotation1.Equals("---"))
+                    {
+                        obm.Rotation1 = GetProject(r.P_Id);
+                    }
+                    else if (obm.Rotation2.Equals("---"))
+                    {
+                        obm.Rotation2 = GetProject(r.P_Id);
+                    }
+                    else if (obm.Rotation3.Equals("---"))
+                    {
+                        obm.Rotation3 = GetProject(r.P_Id);
+                    }
+                    else if (obm.Rotation4.Equals("---"))
+                    {
+                        obm.Rotation4 = GetProject(r.P_Id);
+                    }
+
+                }
+                details.Add(obm);
+
+            }
+
+
+            return View(details);
+
+        }
+        public String GetProject(int ProjectId)
+        {
+            Project project = ctx.Projects.FirstOrDefault(m => m.P_Id == ProjectId);
+            return project.P_Name;
+        }
         public JsonResult RemoteDataSource_GetProducts(string text)
         {
 
